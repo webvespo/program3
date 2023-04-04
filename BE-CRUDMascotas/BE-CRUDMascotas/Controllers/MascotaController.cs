440d01d410d1dea1493aea4d9d700d82b489deb3
Daniel Gustavo Vespo <gustavovespo@gmail.com>
2023-04-04 01:08:44 -0300

    agregado de BE

---
+++ BE-CRUDMascotas/BE-CRUDMascotas/Controllers/MascotaController.cs
@@ -0,0 +1,32 @@
+﻿using BE_CRUDMascotas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+
+namespace BE_CRUDMascotas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MascotaController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+        public MascotaController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listMascotas = await _context.Mascotas.ToListAsync();
+                return Ok(listMascotas);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ BE-CRUDMascotas/BE-CRUDMascotas/Models/Mascota.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace BE_CRUDMascotas.Models
+{
+    public class Mascota
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Raza { get; set; }
+        public string Color { get; set; }
+        public int Edad { get; set; }
+        public float Peso { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
